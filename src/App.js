9bc0efabc7fa5775e0f8b41269bc13a8d9210f93
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-05-04 20:28:55 -0700

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
+
+
+const toDoArray = [
+  {
+    id: 12, 
+    work: 'Printing',
+    done: false
+  }
+];
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    constructor() {
+      super();
+      this.state = {
+        toDoArray: toDoArray
+      };
+    }
+
+    // for selecting the toDo
+
+    toggleTodo = toId => {
+      this.setState({
+        toDoArray: this.state.toDoArray.map(toDo => {
+          if (toDo.id === todoId) {
+            return {
+              ...toDo,
+              done: !toDo.done
+            };
+          }
+          return toDo;
+        })
+      });
+    };
+
+    // adding ToDo 
+
+    addTodo = toDoName => {
+      this.setState({
+        toDoArray: [
+          ...this.state.toDoArray, 
+          {
+            work: toDoName,
+            done: false,
+            id: (Math.random() * Math.random()).toString(9).subtr(2, 9)
+          }
+        ]
+      });
+    };
+
+// clear Todo 
+clearTodo = () => {
+  this.setState({
+    toDoArray: this.state.toDoArray.filter(toDo => {
+      return !toDo.done;
+    })
+  });
+};
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addTodo={this.addTodo} />
+        <TodoList
+        toDoArray={this.statetoDoArray}
+        toggleTodo={this.toggleTodo}
+        clearTdo={this.clearTodo}
+        />
       </div>
     );
   }
